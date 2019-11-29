a5d031c0fb863a00292abfab422476938f9232f8
Alex Felipe <alexfelipevieira@gmail.com>
2019-11-29 10:52:42 -0200

    2.1

--- lib/http/webclients/transaction_webclient.dart
+++ lib/http/webclients/transaction_webclient.dart
@@ -24,6 +24,14 @@ class TransactionWebClient {
         },
         body: transactionJson);
 
+    if(response.statusCode == 400){
+      throw Exception('there was an error submitting transaction');
+    }
+
+    if(response.statusCode == 401){
+      throw Exception('authentication failed');
+    }
+
     return Transaction.fromJson(jsonDecode(response.body));
   }
 
--- lib/screens/transaction_form.dart
+++ lib/screens/transaction_form.dart
@@ -94,6 +94,8 @@ class _TransactionFormState extends State<TransactionForm> {
       if (transaction != null) {
         Navigator.pop(context);
       }
+    }).catchError((e){
+      print(e);
     });
   }
 }
