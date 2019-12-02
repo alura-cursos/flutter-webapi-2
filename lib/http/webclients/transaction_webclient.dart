692650798c19b084157d34fe741403a1f2a9ab79
Alex Felipe <alexfelipevieira@gmail.com>
2019-12-02 11:35:06 -0200

    4.1

--- lib/http/webclients/transaction_webclient.dart
+++ lib/http/webclients/transaction_webclient.dart
@@ -17,6 +17,8 @@ class TransactionWebClient {
   Future<Transaction> save(Transaction transaction, String password) async {
     final String transactionJson = jsonEncode(transaction.toJson());
 
+    await Future.delayed(Duration(seconds: 10));
+
     final Response response = await client.post(baseUrl,
         headers: {
           'Content-type': 'application/json',
